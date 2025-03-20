87b5c9bccbe8e8d7432e226b28f92c524fd3295a
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-03-20 22:51:26 +0500

    integrating frontend

--- frontend/app/auction/[slug]/page.js
+++ frontend/app/auction/[slug]/page.js
@@ -5,7 +5,7 @@ import Breadcrumb2 from "../../components/common/Breadcrumb2";
 import AuctionGallery from "../../components/auction-gallary/AuctionGallery";
 import React, { useEffect, useMemo, useState, useContext } from "react";
 import ModalVideo from "react-modal-video";
-import { UserContext } from '../../contexts/UserContext'; 
+//import { UserContext } from '../../contexts/UserContext'; 
 //import { connectSocket, emitBid, listenForNewBids } from "../../components/socket";
 import CountdownTimer from '../../components/CountdownTimer';
 import { Swiper, SwiperSlide } from "swiper/react";
