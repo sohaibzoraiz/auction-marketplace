99bb9e066189e9956a87ed50ad0cba5a0d749687
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-03-22 22:35:52 +0500

    integrating frontend

--- frontend/app/auctions/page.js
+++ frontend/app/auctions/page.js
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useEffect } from 'react';
 import AllListings from '../components/auction-components/AllListings';
 import Breadcrumb2 from "@/components/common/Breadcrumb2";
