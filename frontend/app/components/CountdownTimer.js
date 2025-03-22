af1d3ec7b2a80976e78bdc4308c993f14d2582c1
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-03-22 16:47:54 +0500

    integrating frontend

--- frontend/app/components/CountdownTimer.js
+++ frontend/app/components/CountdownTimer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
 import { useCountdownTimer } from '@/customHooks/useCountdownTimer';
 
 function CountdownTimerComponent({ endTime }) {
