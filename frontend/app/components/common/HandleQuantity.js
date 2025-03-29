78c927c505270c90875858980f778604022d978d
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-03-30 01:20:53 +0500

    integrating frontend

--- frontend/app/components/common/HandleQuantity.js
+++ frontend/app/components/common/HandleQuantity.js
@@ -77,7 +77,7 @@ function HandleQuantity({ currentPrice, onQuantityChange, lastBidFromDB }) {
     if (lastBidFromDB && Math.floor(lastBidFromDB) !== state.minLimit+10000) {
       dispatch({ type: "UPDATE_LIMITS", payload: lastBidFromDB });
     }
-  }, [lastBidFromDB]);
+  }, [lastBidFromDB,state.minLimit]);
 
   const increment = () => dispatch({ type: "INCREMENT" });
   const decrement = () => dispatch({ type: "DECREMENT" });
