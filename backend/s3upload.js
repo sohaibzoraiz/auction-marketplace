28c34ea479b267b0707854ca7a33fb6b79c3f43e
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-04-03 17:38:21 +0500

    integrating frontend

--- backend/s3upload.js
+++ backend/s3upload.js
@@ -8,7 +8,10 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Set in your .env
   region: process.env.AWS_REGION,                  // e.g., 'us-east-1'
 });
-
+console.log("AWS_REGION:", process.env.AWS_REGION);
+console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
+console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
+console.log("AWS_S3_BUCKET:", process.env.AWS_S3_BUCKET);
 const s3 = new AWS.S3();
 
 const upload = multer({
