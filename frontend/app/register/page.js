b4929b5c125487fbeca415732ce978fdcea296f8
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-04-05 01:05:22 +0500

    integrating frontend

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 //import { useRouter } from "next/navigation";
 import Modal from "../components/auction-single/modal";
