fb028101dbc8eac314f2824266e5fd979780e1dc
Sohaib Murtaz <sohaib.zoraiz@gmail.com>
2025-04-06 03:38:06 +0500

    integrating frontend

--- backend/routes/dropdownRoutes.js
+++ backend/routes/dropdownRoutes.js
@@ -4,8 +4,8 @@ const dropdownController = require('../controllers/dropdownController');
 
 router.get('/makes', dropdownController.getMakes);
 router.get('/models', dropdownController.getModelsByMake);
-router.get('/variants', dropdownController.getVariantsByModel);
-router.get('/years', dropdownController.getYearRangeByVersion);
+//router.get('/variants', dropdownController.getVariantsByModel);
+//router.get('/years', dropdownController.getYearRangeByVersion);
 router.get('/variants', dropdownController.getVariantsByModelAndYear);
 router.get('/years', dropdownController.getYearRangesByModel);
 
